4da9a00b6582ff5d01e101eef67e347926796f62
JEYASRI S <108861190+jeyasri-senthil@users.noreply.github.com>
2024-01-16 00:16:48 +0530

    Update BinarySearch.java

--- Day_13/BinarySearch.java
+++ Day_13/BinarySearch.java
@@ -15,3 +15,17 @@ class Solution {
     }
 }
 
+// Recursive Approach
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length, low = 0, high = n-1;
+        return binarySearch(nums, low, high, target);
+    }
+    public static int binarySearch(int[] nums, int low, int high, int target){
+        if(low > high) return -1;
+        int mid = (low + high) / 2;
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] > target) return binarySearch(nums, low, mid-1, target);
+        else return binarySearch(nums, mid+1, high, target);
+    }
+}
