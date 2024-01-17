63b1ba9cb4c8946a8c5b5fe7a421ec22dfb96759
JEYASRI S <108861190+jeyasri-senthil@users.noreply.github.com>
2024-01-17 22:13:41 +0530

    Create NumberOfOccurrence.java

---
+++ Day_14/NumberOfOccurrence.java
@@ -0,0 +1,37 @@
+// Problem Link: https://www.codingninjas.com/studio/problems/occurrence-of-x-in-a-sorted-array_630456
+
+public class Solution {
+    public static int count(int[] arr, int n, int x) {
+        //Your code goes here
+        int firstOccurrence = firstOccurrence(arr, x);
+        int lastOccurrence = lastOccurrence(arr, x);
+        if(firstOccurrence == -1 || lastOccurrence == -1) return 0;
+        return lastOccurrence - firstOccurrence + 1;
+    }
+    public static int firstOccurrence(int[] arr, int x) {
+        int first = -1, low = 0, high = arr.length-1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(arr[mid] == x) {
+                first = mid;
+                high = mid-1;
+            }
+            else if(arr[mid] < x) low = mid+1;
+            else high = mid-1;
+            }
+        return first;
+    }
+    public static int lastOccurrence(int[] arr, int x) {
+        int last = -1, low = 0, high = arr.length-1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(arr[mid] == x) {
+                last = mid;
+                low = mid+1;
+            }
+            else if(arr[mid] < x) low = mid+1;
+            else high = mid-1;
+            }
+        return last;
+    }
+}
