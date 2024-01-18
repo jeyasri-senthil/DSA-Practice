1303a9a71b992c304d48c0086e0ee42d96be78e1
JEYASRI S <108861190+jeyasri-senthil@users.noreply.github.com>
2024-01-18 23:31:23 +0530

    Create Pascal'sTriangle.java

---
+++ Day_15/Pascal'sTriangle.java
@@ -0,0 +1,56 @@
+// Problem Link: https://leetcode.com/problems/pascals-triangle/
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int row=1; row<=numRows; row++) {
+            List<Integer> tempList = new ArrayList<>();
+            for(int col=1; col<=row; col++) {
+                tempList.add(functionNCR(row-1, col-1));
+            }
+            ans.add(tempList);
+        }
+        return ans;
+    }
+    public static int functionNCR(int n, int r) {
+        int res = 1;
+        for(int i=0; i<r; i++) {
+            res = res*(n-i);
+            res = res/(i+1);
+        }
+        return res;
+    }
+}
+
+// Problem Link: https://www.codingninjas.com/studio/problems/print-pascal-s-triangle_6917910
+
+import java.util.*;
+public class Solution {
+    public static int[][] pascalTriangle(int N) {
+        // Write your code here.
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int row=1; row<=N; row++) {
+            List<Integer> tempList = new ArrayList<>();
+            for(int col=1; col<=row; col++) {
+                tempList.add(functionNCR(row-1, col-1));
+            }
+            ans.add(tempList);
+        }
+        int[][] result = new int[N][];
+        for(int i=0; i<N; i++) {
+            result[i] = new int[ans.get(i).size()];
+            for(int j=0; j<ans.get(i).size(); j++) {
+                result[i][j] = ans.get(i).get(j);
+            }
+        }
+        return result;
+    }
+    public static int functionNCR(int n, int r) {
+        int res = 1;
+        for(int i=0; i<r; i++) {
+            res = res*(n-i);
+            res = res/(i+1);
+        }
+        return res;
+    }
+}
