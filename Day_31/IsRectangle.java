726c20bb834b8c8824d9f43ffd3db93bd6f5bc0d
JEYASRI S <108861190+jeyasri-senthil@users.noreply.github.com>
2024-02-20 11:44:28 +0530

    Create IsRectangle.java

---
+++ Day_31/IsRectangle.java
@@ -0,0 +1,8 @@
+// Problem Link: https://www.interviewbit.com/problems/is-rectangle/
+
+public class Solution {
+    public int solve(int A, int B, int C, int D) {
+        if(A == B && C == D) return 1;
+        else return 0;
+    }
+}
