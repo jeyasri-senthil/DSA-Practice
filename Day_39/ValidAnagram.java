f41c0ed4e984c30e9475658f7cfbba04fa9e1b1d
JEYASRI S <108861190+jeyasri-senthil@users.noreply.github.com>
2024-04-01 11:38:51 +0530

    Create ValidAnagram.java

---
+++ Day_39/ValidAnagram.java
@@ -0,0 +1,13 @@
+// Problem Link: https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] freq = new int[26];
+        for(char c : s.toCharArray()) freq[c-'a']++;
+        for(char c : t.toCharArray()) freq[c-'a']--;
+        for(int i=0; i<26; i++) {
+            if(freq[i] != 0) return false;
+        }
+        return true;
+    }
+}
